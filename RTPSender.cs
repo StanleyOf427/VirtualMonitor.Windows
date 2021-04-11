dfa482156ab643c23eaf5fb1e73f13797a95c17c
Stanley No.427 <StanleyOf427@hotmail.com>
2021-04-11 16:42:27 +0800

    Add project files.

---
+++ RTPSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMonitor_S
+{
+    class RTPSender
+    {
+        private string _rtpPackage;//[$],[channel],[size],[size],[RTP Header],[RTP Playload]
+        public uint BufSize;
+        public struct RTPPackage
+        {
+            char[] header;
+            RTPHeader rtpHeader;
+            char[] rtpPayload;
+        }
+        public struct RTPHeader
+        {
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
